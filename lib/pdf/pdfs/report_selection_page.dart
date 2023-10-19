f987c41e768cad111727dbde89514c2d08835bf3
7skiessolution <7skiessolutions@gmail.com>
2023-10-19 13:38:48 -0700

    test

--- lib/pdf/pdfs/report_selection_page.dart
+++ lib/pdf/pdfs/report_selection_page.dart
@@ -78,6 +78,198 @@ class _ReportSelectionPageState extends State<ReportSelectionPage> {
                       color: white,
                       child: Column(
                         children: [
+                          Text(
+                            "Compare the values of the Report",
+                            style: GoogleFonts.poppins(
+                                // color: white,
+                                fontSize: width * 0.03,
+                                fontWeight: FontWeight.w400),
+                          ),
+                          SizedBox(
+                            height: height * 0.02,
+                          ),
+                          // Padding(
+                          //   padding: EdgeInsets.only(
+                          //     left: width * 0.08,
+                          //   ),
+                          //   child: Row(
+                          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          //     children: [
+                          //       Text(
+                          //         AppLocalizations.of(context)!
+                          //             .type
+                          //             .capitalize!,
+                          //         style: GoogleFonts.poppins(
+                          //             color: Colors.black,
+                          //             fontSize: width * 0.032,
+                          //             fontWeight: FontWeight.w500),
+                          //       ),
+                          //       Container(
+                          //         height: height * 0.04,
+                          //         width: width * 0.25,
+                          //         decoration: BoxDecoration(
+                          //             color: Colors.grey.withOpacity(0.5)),
+                          //         child: DropdownButtonHideUnderline(
+                          //           child: DropdownButton<String>(
+                          //             dropdownColor: Colors.white,
+                          //             focusColor: Colors.black,
+                          //             value: obj.selectedType,
+                          //             style: TextStyle(
+                          //                 color: Colors.black,
+                          //                 fontSize: size.width * 0.035),
+                          //             onChanged: (newValue) {
+                          //               obj.selectedType = newValue as String;
+                          //               obj.update();
+
+                          //               if (obj.startdate != null) {
+                          //                 obj.getcategory(
+                          //                     obj.selectedType == "Sale"
+                          //                         ? 1
+                          //                         : obj.selectedType ==
+                          //                                 "Expense"
+                          //                             ? 2
+                          //                             : obj.selectedType ==
+                          //                                     "Purchase"
+                          //                                 ? 0
+                          //                                 : 3,
+                          //                     obj.startdate!,
+                          //                     obj.enddate);
+                          //               }
+
+                          //               print(
+                          //                   "=-=-==-=-=- ${obj.selectedType}");
+                          //             },
+                          //             items: obj.types
+                          //                 .map<DropdownMenuItem<String>>(
+                          //                     (String value) {
+                          //               return DropdownMenuItem<String>(
+                          //                 value: value,
+                          //                 child: Text(
+                          //                   value,
+                          //                   style: TextStyle(
+                          //                       color: Colors.black,
+                          //                       fontSize: width * 0.035),
+                          //                 ),
+                          //               );
+                          //             }).toList(),
+                          //             hint: Text(AppLocalizations.of(context)!
+                          //                 .selecttype
+                          //                 .capitalize!),
+                          //             icon: const Icon(
+                          //               Icons.arrow_drop_down,
+                          //               color: Colors.black,
+                          //             ),
+                          //             elevation: 1,
+                          //             isExpanded: true,
+                          //             isDense: true,
+                          //             selectedItemBuilder:
+                          //                 (BuildContext context) {
+                          //               return obj.types
+                          //                   .map<Widget>((String value) {
+                          //                 return Container(
+                          //                   alignment: Alignment.centerLeft,
+                          //                   child: Padding(
+                          //                     padding:
+                          //                         const EdgeInsets.symmetric(
+                          //                             horizontal: 5.0),
+                          //                     child: Text(
+                          //                       value,
+                          //                       style: GoogleFonts.poppins(
+                          //                           color: Colors.black,
+                          //                           fontSize: width * 0.032,
+                          //                           fontWeight:
+                          //                               FontWeight.w400),
+                          //                     ),
+                          //                   ),
+                          //                 );
+                          //               }).toList();
+                          //             },
+                          //           ),
+                          //         ),
+                          //       ),
+                          //     ],
+                          //   ),
+                          // ),
+                          SizedBox(
+                            height: height * 0.01,
+                          ),
+                          // Padding(
+                          //   padding: EdgeInsets.only(
+                          //       left: width * 0.08, right: width * 0.1),
+                          //   child: Row(
+                          //     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          //     children: [
+                          //       Text(
+                          //         AppLocalizations.of(context)!
+                          //             .from
+                          //             .capitalize!
+                          //             .toUpperCase(),
+                          //         style: GoogleFonts.poppins(
+                          //             color: blue,
+                          //             fontSize: width * 0.032,
+                          //             fontWeight: FontWeight.w500),
+                          //       ),
+                          //       Text(
+                          //         AppLocalizations.of(context)!
+                          //             .to
+                          //             .capitalize!
+                          //             .toUpperCase(),
+                          //         style: GoogleFonts.poppins(
+                          //             color: blue,
+                          //             fontSize: width * 0.032,
+                          //             fontWeight: FontWeight.w500),
+                          //       ),
+                          //     ],
+                          //   ),
+                          // ),
+                          SizedBox(
+                            height: height * 0.01,
+                          ),
+                          // Row(
+                          //   mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          //   children: [
+                          //     Container(
+                          //       height: height * 0.05,
+                          //       width: width * 0.25,
+                          //       alignment: Alignment.center,
+                          //       decoration: BoxDecoration(
+                          //           color: Colors.grey.withOpacity(0.5)),
+                          //       child: TextButton(
+                          //           onPressed: () {
+                          //             // obj.selectDate(context, true);
+                          //           },
+                          //           child: Text(
+                          //             !obj.dateFromPicked
+                          //                 ? AppLocalizations.of(context)!.select
+                          //                 : obj.startdate.toString(),
+                          //             style: GoogleFonts.poppins(
+                          //                 color: Colors.black,
+                          //                 fontSize: width * 0.032,
+                          //                 fontWeight: FontWeight.w500),
+                          //           )),
+                          //     ),
+                          //     Container(
+                          //       height: height * 0.05,
+                          //       width: width * 0.25,
+                          //       alignment: Alignment.center,
+                          //       decoration: BoxDecoration(
+                          //           color: Colors.grey.withOpacity(0.5)),
+                          //       child: TextButton(
+                          //           onPressed: () {
+                          //             //  obj.selectDate(context, false);
+                          //           },
+                          //           child: Text(
+                          //             !obj.dateToPicked
+                          //                 ? AppLocalizations.of(context)!.select
+                          //                 : obj.enddate.toString(),
+                          //             style: GoogleFonts.poppins(
+                          //                 color: Colors.black,
+                          //                 fontSize: width * 0.032,
+                          //                 fontWeight: FontWeight.w500),
+                          //           )),
+                          //     ),
+                          //   ],
+                          // ),
                           InkWell(
                             onTap: () {
                               Navigator.push(
@@ -110,6 +302,8 @@ class _ReportSelectionPageState extends State<ReportSelectionPage> {
                           ),
                           InkWell(
                             onTap: () {
+                              // PdfController.to.fetchcompenysales();
+
                               Navigator.push(
                                   context,
                                   MaterialPageRoute(
